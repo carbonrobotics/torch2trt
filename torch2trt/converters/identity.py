745036edd90ee776bc3c3c7d72142be34976f2ef
Evan Brossard <evanbrossard@gmail.com>
2024-01-02 14:10:16 -0800

    Add to

--- torch2trt/converters/identity.py
+++ torch2trt/converters/identity.py
@@ -20,3 +20,19 @@ def convert_identity(ctx):
     input_trt = add_missing_trt_tensors(ctx.network, [input])[0]
     output = ctx.method_return
     output._trt = input_trt
+
+@tensorrt_converter('torch.Tensor.to')
+def convert_identity(ctx):
+    input = ctx.method_args[0]
+    input_trt = add_missing_trt_tensors(ctx.network, [input])[0]
+    torch_type = ctx.method_args[1]
+    tensorrt_type = trt.DataType.FLOAT
+    if torch_type == torch.float16:
+        tensorrt_type = trt.DataType.HALF
+    elif torch_type == torch.int8:
+        tensorrt_type = trt.DataType.INT8
+
+    layer = ctx.network.add_identity(input_trt)
+    layer.set_output_type(0, tensorrt_type)
+    output = ctx.method_return[0]
+    output._trt = layer.get_output(0)
